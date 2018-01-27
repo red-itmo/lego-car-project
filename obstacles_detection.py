30a86c8cd84c08af16168b1ec1dab2bf8d3fd0de
Uebuntu2.0 <rami.naim2010@yandex.ru>
2018-01-27 17:50:11 +0300

    obstacle detection made with class

--- obstacles_detection.py
+++ obstacles_detection.py
@@ -19,48 +19,20 @@ class Mapping:
 		mask = cv2.erode(mask, None, iterations=3)
 		# Get mask of the image with founded edges
 		edges = cv2.Canny( mask, 100, 200 )
-		# Get all contours
+		# Get all counters
 		obst_cntrs = cv2.findContours( edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS )[-2]
 
-		### TODO: FILTER CONTOURS IN ORDER TO GET RID OF SMALL NOISE AND STUFF ( LINE 47 )
+		# Following loop deletes small contours
+		obstacles = []
+		for o in obst_cntrs:
+			if cv2.contourArea(o) > 500:
+				addToList = True
+				obst_coord = cv2.minAreaRect( o )
+				obst_coord = cv2.boxPoints( obst_coord )
+				obst_coord = np.int0( np.around(obst_coord) )
+				first_point = [ obst_coord[0] ]
+				obst_coord = np.concatenate( (obst_coord, first_point ) )
+				obstacles.append( obst_coord )
 
-		return obst_cntrs
 
-
-"""while True:
-	#ret, frame = camera.read()
-	frame = cv2.imread("floor_with_obsticles.png")
-	ret = True
-
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-	blurred = cv2.bilateralFilter( gray, 12, 100, 100 )
-	_, mask = cv2.threshold( blurred, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU )
-
-	mask = cv2.erode(mask, None, iterations=3)
-
-	edges = cv2.Canny( mask, 100, 200 )
-	obst_cntrs = cv2.findContours( edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS )[-2]
-
-	if obst_cntrs:
-		for cntr in obst_cntrs:
-			if cv2.contourArea( cntr ) > 450:
-				obst_coord = cv2.minAreaRect( cntr )
-				points = cv2.boxPoints( obst_coord )
-				points = np.int0( np.around(points) )
-				print points
-				for i in range( -1, len(points) - 1 ):
-					cv2.line(frame, tuple(points[i]), tuple(points[i+1]), (255, 0, 0), 3)
-
-	if not ret:
-		break
-
-	if cv2.waitKey(1) & 0xFF is ord('q'):
-		cv2.destroyAllWindows()
-		break
-
-	cv2.imshow("Frame", frame)
-	cv2.imshow("Edges", edges)
-	cv2.imshow("Mask", mask)
-
-print obst_cntrs"""
+		return obstacles
