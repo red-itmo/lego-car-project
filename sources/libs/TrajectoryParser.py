a32e30ee99c6f745b3e14427a5cda315003e39c9
RamiNaim <rami.naim2010@yandex.ru>
2018-04-16 21:50:05 +0300

    TrajectoryParser fixed (probably)

--- sources/libs/TrajectoryParser.py
+++ sources/libs/TrajectoryParser.py
@@ -1,12 +1,21 @@
-class TrajectoryParser:
-    self.init_Pose = NULL
+from math import sin, cos
+from numpy import asscalar
+from TrajectoryStuff import Point, Pose, ClothoidLine, StraightLine, CircleLine
 
+
+# Class for encoding the trajectory
+class TrajectoryParser:
+
+    # Constructor gets initial pose of a robot as an argument
+    # pose = [ x, y, angle ]
     def __init__(self, initP):
         self.init_Pose = Pose(initP[0], initP[1], initP[2])
 
     def setInitPose(self, initP):
         self.init_Pose = Pose(initP[0], initP[1], initP[2])
 
+    # Method transform point in robot's system of coordinates
+    # TODO: It might be that the problem in here
     def transformPoint(self, point):
         theta = self.init_Pose.angle
         x_init = self.init_Pose.point.x
@@ -18,6 +27,8 @@ class TrajectoryParser:
         y = -cos(theta) * frame_y - sin(theta) * frame_x + cos(theta) * y_init + sin(theta) * x_init
         return Point(x, y)
 
+    # Method transform point in robot's system of coordinates
+    # TODO: It might be that the problem in here (with angle)
     def transformPose(self, pose):
         theta = self.init_Pose.angle
         angle = -pose.angle
@@ -30,15 +41,17 @@ class TrajectoryParser:
         y = -cos(theta) * frame_y - sin(theta) * frame_x + cos(theta) * y_init + sin(theta) * x_init
         return Pose(x, y, angle)
 
+    # Main function gets trajectory descriptor and returns transformed version of it
     def decode_trajectory(self, path):
         full_trajectory = []
         for traj in path:
-            print(traj)
+
             if traj[0] == 'ClothoidLine':
                 f_pose = Pose(traj[1][0, 0], traj[1][0, 1], traj[1][0, 2])
                 pose = self.transformPose(f_pose)
                 t = ClothoidLine(pose, traj[2][0], -traj[3][0], traj[4][0], traj[6][0], type=traj[5])
                 full_trajectory.append(t)
+
             elif traj[0] == 'CircleLine':
                 f_centerPoint = Point(traj[1][0, 0], traj[1][0, 1])
                 f_startPoint = Point(traj[2][0, 0], traj[2][0, 1])
