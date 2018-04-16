632c0c59c044fd005435423481426e71acfce2fd
AlexKaravaev <30314738+AlexKaravaev@users.noreply.github.com>
2018-04-16 12:35:34 +0300

    check for correct msg

--- sources/libs/Localization.py
+++ sources/libs/Localization.py
@@ -11,13 +11,13 @@ class Localization:
 	def __init__(self, car, init_pose):
 		self.__car = car
 		self.__x = 0
-		self.__y = 0
 		self.x_car = 0
 		self.y_car = 0
 		self.x_init = init_pose[0]
 		self.y_init = init_pose[1]
+		self.__y = 0
 		self.alpha = init_pose[2]
-		print("X_init {}, Y_init {}, Angle_init {}".format(self.x_init, self.y_init, self.alpha))
+		#print("X_init {}, Y_init {}, Angle_init {}".format(self.x_init, self.y_init, self.alpha))
 
 	def getData(self, theta, motor_speed, dt):
 		alpha = copy.deepcopy(self.alpha)
@@ -38,12 +38,14 @@ class Localization:
 		else:
 			while True:
 				coord = self.__car.s.get_data()
-				if coord:
+				print(coord)
+				if isinstance(coord[0],float):
 					break
+			print('----------------------------------')
 			print(coord)
 
 			self.__x = self.x_car * 0 + 1 * (-sin(alpha) * coord[1] + cos(alpha) * coord[0] + sin(alpha) * self.y_init - cos(alpha) * self.x_init)
 			self.__y = self.y_car * 0 + 1 * (-cos(alpha) * coord[1] - sin(alpha) * coord[0] + cos(alpha) * self.y_init + sin(alpha) * self.x_init)
-			print("x: {} y: {} x_car: {} y_car: {}".format(self.__x, self.__y, self.x_car, self.y_car))
+			#print("x: {} y: {} x_car: {} y_car: {}".format(self.__x, self.__y, self.x_car, self.y_car))
 
 		return self.__x, self.__y, vx, vy, v, theta
